14c96d286f84d63f8fd96e43ae9ba5697af08e1d
pushkaladlakha <pushkaladlakha3@gmail.com>
2026-07-06 02:56:35 +0530

    Time: 3 ms (9.54%), Space: 9.5 MB (5.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int traverse(int row, int col, int m, int n, vector<vector<int>>& dp) {
-
-        if (row >= m || col >= n)
-            return 0;
-
-        if (row == m - 1 && col == n - 1)
-            return 1;
-
-        if (dp[row][col] != -1)
-            return dp[row][col];
-
-        int down = traverse(row + 1, col, m, n, dp);
-        int right = traverse(row, col + 1, m, n, dp);
-
-        return dp[row][col] = down + right;
-    }
-
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
-
-        return traverse(0, 0, m, n, dp);
+        for(int i = 0 ; i < m ; i++){
+            for(int j=0 ; j < n ; j++){
+                if(i==0 && j==0){dp[0][0]=1;}
+                else{
+                    int up = 0 , left = 0 ;
+                    if(i>0)  up = dp[i-1][j];
+                    if(j>0)  left = dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
